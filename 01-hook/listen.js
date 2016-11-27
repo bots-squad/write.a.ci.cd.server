7ac2d6165f285e8f37881e1922ef8626454307d1
k33g <k33g@github.com>
2016-11-27 09:29:36 +0100

    add some samples

---
+++ 01-hook/listen.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const express = require('express');
+const bodyParser = require('body-parser');
+
+let http_port = 9090;
+
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/ci', (req, res) => {
+
+  let event = req.headers['x-github-event'];
+
+  console.log(`GitHub event: ${event}`);
+  console.log(`Repository: ${req.body.repository.name}`);
+  console.log(`Owner: ${req.body.repository.owner.name}`);
+  console.log(`Ref: ${req.body.ref}`);
+
+  res.status(201).end();
+});
+
+app.listen(http_port)
+
+console.log(`Listening on ${http_port}`);
